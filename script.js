55e819b602428075fa1fe45ccd8b4c6220312d9e
Alesh Patil <aleshpatil24032002@gmail.com>
2024-01-25 11:34:12 +0530

    JS file added

---
+++ script.js
@@ -0,0 +1,5 @@
+let btn=document.querySelector('button');
+       btn.addEventListener('click',showMsg);
+       function showMsg(){
+         alert("Thank You for visiting my profile page !!!");
+       }
